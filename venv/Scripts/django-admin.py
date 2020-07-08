51b12e3e0e144f22fb715fee4bf1e7156843dec0
SLIPPERS <evansonoka05.j@gmail.com>
2020-07-08 11:26:17 +0300

    started ticketing project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user-pc\docume~1\python~1\projects\sticke~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
